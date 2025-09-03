2c8d0242ae77aacdbe7384284c331e11791b7818
Zapper9982 <144029909+Zapper9982@users.noreply.github.com>
2025-09-03 18:57:23 +0530

    Update GenerateBeneficiaryServiceTests.java

--- src/test/java/com/iemr/common/bengen/GenerateBeneficiaryServiceTests.java
+++ src/test/java/com/iemr/common/bengen/GenerateBeneficiaryServiceTests.java
@@ -19,7 +19,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see https://www.gnu.org/licenses/.
 */
-// please work please plljljljlj
+// please work please plljljljlj sd;lfkjasd;lfkja;dslkj;
 package com.iemr.common.bengen;
 
 import org.junit.jupiter.api.Test;
